515197bceb886cc6e2d4e7483b9c448c9f6c8d0d
Advaith Suresh-Nair <AS2866@student.aru.ac.uk>
2022-10-11 14:51:28 +0100

    Idea Generator Login Update

--- IdeaGeneratorLoginLabel.Designer.cs
+++ IdeaGeneratorLoginLabel.Designer.cs
@@ -30,46 +30,87 @@ namespace aru_software_eng_UI
         private void InitializeComponent()
         {
             this.IGLoginLabel = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.IdeaGenerator = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // IGLoginLabel
             // 
-            this.IGLoginLabel.Location = new System.Drawing.Point(101, 260);
+            this.IGLoginLabel.Location = new System.Drawing.Point(120, 316);
             this.IGLoginLabel.Name = "IGLoginLabel";
             this.IGLoginLabel.Size = new System.Drawing.Size(553, 75);
             this.IGLoginLabel.TabIndex = 0;
-            this.IGLoginLabel.Text = "button1";
+            this.IGLoginLabel.Text = "Login";
             this.IGLoginLabel.UseVisualStyleBackColor = true;
+            // 
+            // IdeaGenerator
+            // 
+            this.IdeaGenerator.AutoSize = true;
+            this.IdeaGenerator.Font = new System.Drawing.Font("Agency FB", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IdeaGenerator.Location = new System.Drawing.Point(0, 0);
+            this.IdeaGenerator.Name = "IdeaGenerator";
+            this.IdeaGenerator.Size = new System.Drawing.Size(379, 80);
+            this.IdeaGenerator.TabIndex = 1;
+            this.IdeaGenerator.Text = "Idea Generator";
+            this.IdeaGenerator.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Agency FB", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(662, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 68);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "←";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Agency FB", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Location = new System.Drawing.Point(33, 136);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(157, 80);
-            this.label1.TabIndex = 1;
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
             this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click_1);
             // 
-            // button1
+            // label2
             // 
-            this.button1.Location = new System.Drawing.Point(604, 50);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(172, 136);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(257, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(172, 208);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(257, 20);
+            this.textBox2.TabIndex = 6;
             // 
             // IdeaGeneratorLoginLabel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.IdeaGenerator);
             this.Controls.Add(this.IGLoginLabel);
             this.Name = "IdeaGeneratorLoginLabel";
             this.Text = "IdeaGeneratorLoginLabel";
@@ -81,7 +122,11 @@ namespace aru_software_eng_UI
         #endregion
 
         private System.Windows.Forms.Button IGLoginLabel;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label IdeaGenerator;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
