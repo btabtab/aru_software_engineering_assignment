415f66ed4d737a51925d78209e1e901ffaf71e51
Noel Mukhamejanov <NM1033@student.aru.ac.uk>
2022-10-13 15:48:35 +0100

    Initial Sign Up page

---
+++ Signup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aru_software_eng_UI
+{
+    public partial class Signup : Form
+    {
+        public Signup()
+        {
+            InitializeComponent();
+        }
+
+        private void PasswordLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
