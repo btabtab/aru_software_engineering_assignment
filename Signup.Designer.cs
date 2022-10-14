1cb1c9273fed5d19c0fc771f37ce3428581b68aa
lewisMurch <lewismurch2@gmail.com>
2022-10-14 01:46:16 +0100

    Re-ordered labels and inputs for the create account page Made the password input fields function correctly Added an error output for the user to see what is wrong Added red crosses next to any wrong information inputted Added the ability to signup ONLY if all the conditions are met Slightly re-adjusted the mainMenu button position.

--- Signup.Designer.cs
+++ Signup.Designer.cs
@@ -44,7 +44,11 @@ namespace aru_software_eng_UI
             this.GoBackButton = new System.Windows.Forms.Button();
             this.Account_type = new System.Windows.Forms.Label();
             this.accountTypeSelector = new System.Windows.Forms.ComboBox();
-            this.outputTestLabel = new System.Windows.Forms.Label();
+            this.wrongPassSymbol1 = new System.Windows.Forms.Label();
+            this.wrongPassSymbol2 = new System.Windows.Forms.Label();
+            this.wrongEmailSymbol = new System.Windows.Forms.Label();
+            this.wrongUserSymbol = new System.Windows.Forms.Label();
+            this.errorOutputLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // SignUpButton
@@ -65,7 +69,7 @@ namespace aru_software_eng_UI
             // 
             this.SignUpLabel.AutoSize = true;
             this.SignUpLabel.Font = new System.Drawing.Font("Agency FB", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SignUpLabel.Location = new System.Drawing.Point(11, 9);
+            this.SignUpLabel.Location = new System.Drawing.Point(-8, -5);
             this.SignUpLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.SignUpLabel.Name = "SignUpLabel";
             this.SignUpLabel.Size = new System.Drawing.Size(451, 80);
@@ -85,10 +89,11 @@ namespace aru_software_eng_UI
             // LoginGetter
             // 
             this.LoginGetter.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.LoginGetter.Location = new System.Drawing.Point(187, 154);
+            this.LoginGetter.Location = new System.Drawing.Point(445, 115);
             this.LoginGetter.Margin = new System.Windows.Forms.Padding(2);
+            this.LoginGetter.MaxLength = 20;
             this.LoginGetter.Name = "LoginGetter";
-            this.LoginGetter.Size = new System.Drawing.Size(429, 38);
+            this.LoginGetter.Size = new System.Drawing.Size(224, 38);
             this.LoginGetter.TabIndex = 3;
             // 
             // RulesText
@@ -106,7 +111,7 @@ namespace aru_software_eng_UI
             // 
             this.LoginLabel.AutoSize = true;
             this.LoginLabel.Font = new System.Drawing.Font("Agency FB", 20F, System.Drawing.FontStyle.Bold);
-            this.LoginLabel.Location = new System.Drawing.Point(77, 154);
+            this.LoginLabel.Location = new System.Drawing.Point(344, 116);
             this.LoginLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.LoginLabel.Name = "LoginLabel";
             this.LoginLabel.Size = new System.Drawing.Size(106, 34);
@@ -116,10 +121,10 @@ namespace aru_software_eng_UI
             // EmailGetter
             // 
             this.EmailGetter.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.EmailGetter.Location = new System.Drawing.Point(187, 196);
+            this.EmailGetter.Location = new System.Drawing.Point(187, 156);
             this.EmailGetter.Margin = new System.Windows.Forms.Padding(2);
             this.EmailGetter.Name = "EmailGetter";
-            this.EmailGetter.Size = new System.Drawing.Size(429, 38);
+            this.EmailGetter.Size = new System.Drawing.Size(482, 38);
             this.EmailGetter.TabIndex = 6;
             this.EmailGetter.TextChanged += new System.EventHandler(this.EmailGetter_TextChanged);
             // 
@@ -127,7 +132,7 @@ namespace aru_software_eng_UI
             // 
             this.EmailLabel.AutoSize = true;
             this.EmailLabel.Font = new System.Drawing.Font("Agency FB", 20F, System.Drawing.FontStyle.Bold);
-            this.EmailLabel.Location = new System.Drawing.Point(119, 200);
+            this.EmailLabel.Location = new System.Drawing.Point(110, 157);
             this.EmailLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.EmailLabel.Name = "EmailLabel";
             this.EmailLabel.Size = new System.Drawing.Size(64, 34);
@@ -137,17 +142,22 @@ namespace aru_software_eng_UI
             // PasswordGetter1
             // 
             this.PasswordGetter1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PasswordGetter1.Location = new System.Drawing.Point(187, 238);
+            this.PasswordGetter1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.PasswordGetter1.Location = new System.Drawing.Point(187, 199);
             this.PasswordGetter1.Margin = new System.Windows.Forms.Padding(2);
+            this.PasswordGetter1.MaxLength = 14;
             this.PasswordGetter1.Name = "PasswordGetter1";
-            this.PasswordGetter1.Size = new System.Drawing.Size(429, 38);
+            this.PasswordGetter1.PasswordChar = '*';
+            this.PasswordGetter1.Size = new System.Drawing.Size(482, 38);
             this.PasswordGetter1.TabIndex = 8;
+            this.PasswordGetter1.UseSystemPasswordChar = true;
+            this.PasswordGetter1.TextChanged += new System.EventHandler(this.PasswordGetter1_TextChanged);
             // 
             // PasswordLabel
             // 
             this.PasswordLabel.AutoSize = true;
             this.PasswordLabel.Font = new System.Drawing.Font("Agency FB", 20F, System.Drawing.FontStyle.Bold);
-            this.PasswordLabel.Location = new System.Drawing.Point(77, 238);
+            this.PasswordLabel.Location = new System.Drawing.Point(68, 199);
             this.PasswordLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.PasswordLabel.Name = "PasswordLabel";
             this.PasswordLabel.Size = new System.Drawing.Size(106, 34);
@@ -159,7 +169,7 @@ namespace aru_software_eng_UI
             // 
             this.AgainPasswordLabel.AutoSize = true;
             this.AgainPasswordLabel.Font = new System.Drawing.Font("Agency FB", 20F, System.Drawing.FontStyle.Bold);
-            this.AgainPasswordLabel.Location = new System.Drawing.Point(15, 280);
+            this.AgainPasswordLabel.Location = new System.Drawing.Point(6, 242);
             this.AgainPasswordLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.AgainPasswordLabel.Name = "AgainPasswordLabel";
             this.AgainPasswordLabel.Size = new System.Drawing.Size(168, 34);
@@ -169,11 +179,15 @@ namespace aru_software_eng_UI
             // PasswordGetter2
             // 
             this.PasswordGetter2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PasswordGetter2.Location = new System.Drawing.Point(187, 280);
+            this.PasswordGetter2.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.PasswordGetter2.Location = new System.Drawing.Point(187, 245);
             this.PasswordGetter2.Margin = new System.Windows.Forms.Padding(2);
+            this.PasswordGetter2.MaxLength = 14;
             this.PasswordGetter2.Name = "PasswordGetter2";
-            this.PasswordGetter2.Size = new System.Drawing.Size(429, 38);
+            this.PasswordGetter2.PasswordChar = '*';
+            this.PasswordGetter2.Size = new System.Drawing.Size(482, 38);
             this.PasswordGetter2.TabIndex = 11;
+            this.PasswordGetter2.UseSystemPasswordChar = true;
             // 
             // GoBackButton
             // 
@@ -192,7 +206,7 @@ namespace aru_software_eng_UI
             // 
             this.Account_type.AutoSize = true;
             this.Account_type.Font = new System.Drawing.Font("Agency FB", 20F, System.Drawing.FontStyle.Bold);
-            this.Account_type.Location = new System.Drawing.Point(52, 109);
+            this.Account_type.Location = new System.Drawing.Point(44, 114);
             this.Account_type.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Account_type.Name = "Account_type";
             this.Account_type.Size = new System.Drawing.Size(130, 34);
@@ -203,26 +217,71 @@ namespace aru_software_eng_UI
             // 
             this.accountTypeSelector.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
             this.accountTypeSelector.FormattingEnabled = true;
-            this.accountTypeSelector.Location = new System.Drawing.Point(187, 109);
+            this.accountTypeSelector.Location = new System.Drawing.Point(187, 114);
             this.accountTypeSelector.Name = "accountTypeSelector";
-            this.accountTypeSelector.Size = new System.Drawing.Size(429, 39);
+            this.accountTypeSelector.Size = new System.Drawing.Size(153, 39);
             this.accountTypeSelector.TabIndex = 15;
             // 
-            // outputTestLabel
+            // wrongPassSymbol1
             // 
-            this.outputTestLabel.AutoSize = true;
-            this.outputTestLabel.Location = new System.Drawing.Point(689, 399);
-            this.outputTestLabel.Name = "outputTestLabel";
-            this.outputTestLabel.Size = new System.Drawing.Size(84, 13);
-            this.outputTestLabel.TabIndex = 16;
-            this.outputTestLabel.Text = "outputTestLabel";
+            this.wrongPassSymbol1.AutoSize = true;
+            this.wrongPassSymbol1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.wrongPassSymbol1.ForeColor = System.Drawing.Color.Red;
+            this.wrongPassSymbol1.Location = new System.Drawing.Point(674, 202);
+            this.wrongPassSymbol1.Name = "wrongPassSymbol1";
+            this.wrongPassSymbol1.Size = new System.Drawing.Size(0, 31);
+            this.wrongPassSymbol1.TabIndex = 17;
+            // 
+            // wrongPassSymbol2
+            // 
+            this.wrongPassSymbol2.AutoSize = true;
+            this.wrongPassSymbol2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.wrongPassSymbol2.ForeColor = System.Drawing.Color.Red;
+            this.wrongPassSymbol2.Location = new System.Drawing.Point(674, 248);
+            this.wrongPassSymbol2.Name = "wrongPassSymbol2";
+            this.wrongPassSymbol2.Size = new System.Drawing.Size(0, 31);
+            this.wrongPassSymbol2.TabIndex = 18;
+            // 
+            // wrongEmailSymbol
+            // 
+            this.wrongEmailSymbol.AutoSize = true;
+            this.wrongEmailSymbol.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.wrongEmailSymbol.ForeColor = System.Drawing.Color.Red;
+            this.wrongEmailSymbol.Location = new System.Drawing.Point(674, 159);
+            this.wrongEmailSymbol.Name = "wrongEmailSymbol";
+            this.wrongEmailSymbol.Size = new System.Drawing.Size(0, 31);
+            this.wrongEmailSymbol.TabIndex = 19;
+            // 
+            // wrongUserSymbol
+            // 
+            this.wrongUserSymbol.AutoSize = true;
+            this.wrongUserSymbol.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.wrongUserSymbol.ForeColor = System.Drawing.Color.Red;
+            this.wrongUserSymbol.Location = new System.Drawing.Point(674, 118);
+            this.wrongUserSymbol.Name = "wrongUserSymbol";
+            this.wrongUserSymbol.Size = new System.Drawing.Size(0, 31);
+            this.wrongUserSymbol.TabIndex = 20;
+            // 
+            // errorOutputLabel
+            // 
+            this.errorOutputLabel.AutoSize = true;
+            this.errorOutputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.errorOutputLabel.ForeColor = System.Drawing.Color.Red;
+            this.errorOutputLabel.Location = new System.Drawing.Point(285, 301);
+            this.errorOutputLabel.Name = "errorOutputLabel";
+            this.errorOutputLabel.Size = new System.Drawing.Size(0, 31);
+            this.errorOutputLabel.TabIndex = 16;
             // 
             // Signup
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(834, 461);
-            this.Controls.Add(this.outputTestLabel);
+            this.Controls.Add(this.wrongUserSymbol);
+            this.Controls.Add(this.wrongEmailSymbol);
+            this.Controls.Add(this.wrongPassSymbol2);
+            this.Controls.Add(this.wrongPassSymbol1);
+            this.Controls.Add(this.errorOutputLabel);
             this.Controls.Add(this.accountTypeSelector);
             this.Controls.Add(this.Account_type);
             this.Controls.Add(this.GoBackButton);
@@ -263,6 +322,10 @@ namespace aru_software_eng_UI
         private System.Windows.Forms.Label EmailLabel;
         private System.Windows.Forms.Label Account_type;
         private System.Windows.Forms.ComboBox accountTypeSelector;
-        private System.Windows.Forms.Label outputTestLabel;
+        private System.Windows.Forms.Label wrongPassSymbol1;
+        private System.Windows.Forms.Label wrongPassSymbol2;
+        private System.Windows.Forms.Label wrongEmailSymbol;
+        private System.Windows.Forms.Label wrongUserSymbol;
+        private System.Windows.Forms.Label errorOutputLabel;
     }
 }
--- Signup.cs
+++ Signup.cs
@@ -33,40 +33,64 @@ namespace aru_software_eng_UI
         private void SignUpButton_Click(object sender, EventArgs e)
         {
             //sets some variabels equal to each input from the sign up page - L
-            String usernameString = LoginGetter.Text;
-            String passwordString1 = PasswordGetter1.Text;
-            String passwordString2 = PasswordGetter2.Text;
-            String emailEntry = EmailGetter.Text;
+            String username = LoginGetter.Text;
+            String password1 = PasswordGetter1.Text;
+            String password2 = PasswordGetter2.Text;
+            String email = EmailGetter.Text;
+            bool passwordCorrect = false;
+            bool emailCorrect = false;
+            bool usernameCorrect = false;
 
 
-            //EMAIL CODE
-            Boolean emailContain = emailEntry.Contains("@"); //Checks if the text entered into the email entry point contains the correct symbol.
-            if (emailContain == true) //If the email contains the correct symbol - L
-            {
-                outputTestLabel.Text = "Email is valid";
-            }
-            else //If the email does not contain the correct symbol - L
-            {
-                outputTestLabel.Text = "Email is not valid";
-            }
-            //EMAIL CODE
 
             //PASSWORD CODE
-            bool passNotMatch = Convert.ToBoolean(string.Compare(passwordString1, passwordString2)); //Compares if the passwords match - L
+            bool passNotMatch = Convert.ToBoolean(string.Compare(password1, password2)); //Compares if the passwords match - L
             if (passNotMatch == true) //If the passwords do not match, do the following code - L
             {
-                outputTestLabel.Text = "passes are not the same";
+                errorOutputLabel.Text = "passwords do not match"; //Tell the user their password is wrong - L
+                wrongPassSymbol1.Text = "X"; //Add a cross next to the password box - L
+                wrongPassSymbol2.Text = "X"; //Add a cross next to the password repeat box - L
             }
             else //If the passwords do match, do the following code - L
             {
-                outputTestLabel.Text = "passes are the same";
+                errorOutputLabel.Text = ""; //clear the error label - L
+                wrongPassSymbol1.Text = ""; //remove the cross next to the password box - L
+                wrongPassSymbol2.Text = ""; //remove the cross next to the password repeat box - L
+                passwordCorrect = true;
             }
             //PASSWORD CODE
+
+
+            //EMAIL CODE
+            Boolean emailContain = email.Contains("@"); //Checks if the text entered into the email entry point contains the correct symbol - L
+            if (emailContain == true) //If the email contains the correct symbol - L
+            {
+                errorOutputLabel.Text = "Email is valid";
+                wrongEmailSymbol.Text = ""; //remove the cross next to the email repeat box - L
+                emailCorrect = true;
+            }
+
+            else //If the email does not contain the correct symbol - L
+            {
+                errorOutputLabel.Text = "Email is not valid"; //Tells the user their email is invalid - replaces wrong password text as the email is first on the data entry list - L
+                wrongEmailSymbol.Text = "X"; //Add a cross next to the email repeat box - L
+            }
+            //EMAIL CODE
+
+
+            if (passwordCorrect == true & emailCorrect == true)
+            { //if all aspects of the form are correct, run the code below - L
+                errorOutputLabel.Text = ""; //clear the error label - L
+
+            }
+
+
 
         }
 
 
-        private void GoBackButton_Click(object sender, EventArgs e)
+
+private void GoBackButton_Click(object sender, EventArgs e)
         {
 
         }
@@ -82,6 +106,16 @@ namespace aru_software_eng_UI
         }
 
         private void EmailGetter_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PasswordGetter1_TextChanged(object sender, EventArgs e)
         {
 
         }
