d55762cf1a202dbbe8a668bf2cec0ff9e0985d3a
noelkawm <thenoel02@gmail.com>
2022-10-25 13:25:06 +0100

    Started Relatioship View UI

---
+++ RelationshipManagerView.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aru_software_eng_UI
+{
+    public partial class RelationshipManagerView : Form
+    {
+        public RelationshipManagerView()
+        {
+            InitializeComponent();
+        }
+
+        private void RelationshipManagerView_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RiskTrackBar_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CostTrackBar_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExpireDateTrackBar_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TrendingCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
