8188dbaf2c50f9da4e7e599763de7d79b67b864b
btabtab <bladethace@gmail.com>
2022-11-06 02:39:03 +0000

    refactoring

---
+++ FancyDisplayBubble.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aru_software_eng_UI
+{
+	class FancyDisplayBubble
+	{
+		Button button; //Creates a button object for tracking purposes, named button - L
+		int risk; //Risk number for tracking purposes - L
+		int cost;
+		int ID;
+
+		public FancyDisplayBubble(Button n_button, int n_risk, int n_cost, int n_ID) //Constructor, creates a new bubble with a button and risk attribute - L
+		{
+			button = n_button; //Creates new button attribute for bubble - L
+			risk = n_risk; //Creates a risk attribute for the bubble - L
+			cost = n_cost;
+			ID = n_ID;
+			button.Click += new EventHandler(showButtonInfo); //When button is clicked, run the "showButtonInfo" function - L
+		}
+
+		void showButtonInfo(object sender, EventArgs e)
+		{
+			Button target = (Button)sender;
+
+		}
+		public Button getButton() //Encapuslation command to get the buttons reference hidden in fancyDisplayButton - L    
+		{
+			return button;
+		}
+
+		public int getRisk()
+		{
+			return risk;
+		}
+
+		public int getCost()
+		{
+			return cost;
+		}
+
+		public int getID()
+		{
+			return ID;
+		}
+
+	};
+}
---
+++ FancyDisplayBubbleTracker.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aru_software_eng_UI
+{
+	class FancyDisplayBubbleTracker
+	{
+		public static FancyDisplayBubbleTracker instance;
+		List<FancyDisplayBubble> bubbles;
+		//Singleton design pattern esque stuff <3
+		private FancyDisplayBubbleTracker()
+		{
+			bubbles = new List<FancyDisplayBubble>();
+		}
+		public static FancyDisplayBubbleTracker getBubbleTracker()
+		{
+			if (instance == null)
+			{
+				instance = new FancyDisplayBubbleTracker();
+			}
+			return instance;
+		}
+
+		FancyDisplayBubble getBubble(int index)
+		{
+			return bubbles.ElementAt(index);
+		}
+		void addBubble(Button n_button, int risk_factor, int cost_factor, int ID)
+		{
+			bubbles.Add(new FancyDisplayBubble(n_button, risk_factor, cost_factor, ID));
+		}
+
+		FancyDisplayBubble getLastBubble()
+		{
+			return getBubble(bubbles.Count - 1);
+		}
+
+		int getBubbleCount()
+		{
+			return bubbles.Count;
+		}
+		public static FancyDisplayBubble instanceGetBubble(int index)
+		{
+			return getBubbleTracker().getBubble(index);
+		}
+
+		public static void instanceAddBubble(Button n_button, int risk_factor, int cost_factor, int ID)
+		{
+			getBubbleTracker().addBubble(n_button, risk_factor, cost_factor, ID);
+		}
+
+		public static FancyDisplayBubble instanceGetLastBubble()
+		{
+			return getBubbleTracker().getBubble(instance.getBubbleCount() - 1);
+		}
+
+		void deleteButton(object sender, EventArgs e)
+		{
+			Button target = (Button)sender;
+			target.Dispose();
+		}
+
+		public void deleteBubble(int index)
+		{
+			if (getBubbleCount() == 0) { Console.WriteLine("No bubbles left."); return; }
+			if (index < 0) { index = 0; }
+			if (getBubbleCount() <= index) { index = getBubbleCount() - 1; }
+
+			bubbles.RemoveAt(index);
+		}
+
+		public static void instanceDeleteBubble(int index)
+		{
+			getBubbleTracker().deleteBubble(index);
+		}
+		public static void deleteAllBubbles(Form current_form)
+		{
+			while (getBubbleTracker().getBubbleCount() != 0)
+			{
+				current_form.Controls.Remove(instance.getLastBubble().getButton());
+				instance.deleteBubble(getBubbleTracker().getBubbleCount());
+			}
+		}
+	};
+}
