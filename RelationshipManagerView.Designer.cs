c6d17a50fe8d4b6ca5d1c608b5cc0130113401f7
lewisMurch <lewismurch2@gmail.com>
2022-12-02 19:23:30 +0000

    Re-designed RM filter page with added functionality and a consistant design

--- RelationshipManagerView.Designer.cs
+++ RelationshipManagerView.Designer.cs
@@ -29,158 +29,196 @@ namespace aru_software_eng_UI
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem("Bonds");
-            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem("IT");
-            this.RiskTrackBar = new System.Windows.Forms.TrackBar();
-            this.RiskLabel = new System.Windows.Forms.Label();
-            this.CostLabel = new System.Windows.Forms.Label();
-            this.CostTrackBar = new System.Windows.Forms.TrackBar();
-            this.ExpiryDateLabel = new System.Windows.Forms.Label();
-            this.ExpireDateTrackBar = new System.Windows.Forms.TrackBar();
-            this.IndustryLabel = new System.Windows.Forms.Label();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.TrendingCheckBox = new System.Windows.Forms.CheckBox();
+            this.MinRiskLabel = new System.Windows.Forms.Label();
             this.SearchButton = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.RiskTrackBar)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.CostTrackBar)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ExpireDateTrackBar)).BeginInit();
+            this.MaxRiskLabel = new System.Windows.Forms.Label();
+            this.MinCostLabel = new System.Windows.Forms.Label();
+            this.MaxCostLabel = new System.Windows.Forms.Label();
+            this.MinRiskTrackBar = new System.Windows.Forms.TrackBar();
+            this.MaxRiskTrackBar = new System.Windows.Forms.TrackBar();
+            this.MinCostTrackBar = new System.Windows.Forms.TrackBar();
+            this.MaxCostTrackBar = new System.Windows.Forms.TrackBar();
+            this.IndustryLabel = new System.Windows.Forms.Label();
+            this.IndustryInputTextBox = new System.Windows.Forms.TextBox();
+            this.ExpireDateLabel = new System.Windows.Forms.Label();
+            this.ExpireDatePicker = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.MinRiskTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MaxRiskTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MinCostTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MaxCostTrackBar)).BeginInit();
             this.SuspendLayout();
             // 
-            // RiskTrackBar
+            // MinRiskLabel
             // 
-            this.RiskTrackBar.Location = new System.Drawing.Point(319, 130);
-            this.RiskTrackBar.Name = "RiskTrackBar";
-            this.RiskTrackBar.Size = new System.Drawing.Size(301, 45);
-            this.RiskTrackBar.TabIndex = 0;
-            this.RiskTrackBar.Scroll += new System.EventHandler(this.RiskTrackBar_Scroll);
-            // 
-            // RiskLabel
-            // 
-            this.RiskLabel.AutoSize = true;
-            this.RiskLabel.Location = new System.Drawing.Point(191, 140);
-            this.RiskLabel.Name = "RiskLabel";
-            this.RiskLabel.Size = new System.Drawing.Size(28, 13);
-            this.RiskLabel.TabIndex = 1;
-            this.RiskLabel.Text = "Risk";
-            // 
-            // CostLabel
-            // 
-            this.CostLabel.AutoSize = true;
-            this.CostLabel.Location = new System.Drawing.Point(191, 191);
-            this.CostLabel.Name = "CostLabel";
-            this.CostLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.CostLabel.Size = new System.Drawing.Size(28, 13);
-            this.CostLabel.TabIndex = 2;
-            this.CostLabel.Text = "Cost";
-            // 
-            // CostTrackBar
-            // 
-            this.CostTrackBar.Location = new System.Drawing.Point(319, 181);
-            this.CostTrackBar.Name = "CostTrackBar";
-            this.CostTrackBar.Size = new System.Drawing.Size(301, 45);
-            this.CostTrackBar.TabIndex = 3;
-            this.CostTrackBar.Scroll += new System.EventHandler(this.CostTrackBar_Scroll);
-            // 
-            // ExpiryDateLabel
-            // 
-            this.ExpiryDateLabel.AutoSize = true;
-            this.ExpiryDateLabel.Location = new System.Drawing.Point(191, 241);
-            this.ExpiryDateLabel.Name = "ExpiryDateLabel";
-            this.ExpiryDateLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.ExpiryDateLabel.Size = new System.Drawing.Size(62, 13);
-            this.ExpiryDateLabel.TabIndex = 4;
-            this.ExpiryDateLabel.Text = "Expire Date";
-            // 
-            // ExpireDateTrackBar
-            // 
-            this.ExpireDateTrackBar.Location = new System.Drawing.Point(319, 232);
-            this.ExpireDateTrackBar.Name = "ExpireDateTrackBar";
-            this.ExpireDateTrackBar.Size = new System.Drawing.Size(301, 45);
-            this.ExpireDateTrackBar.TabIndex = 5;
-            this.ExpireDateTrackBar.Scroll += new System.EventHandler(this.ExpireDateTrackBar_Scroll);
-            // 
-            // IndustryLabel
-            // 
-            this.IndustryLabel.AutoSize = true;
-            this.IndustryLabel.Location = new System.Drawing.Point(191, 286);
-            this.IndustryLabel.Name = "IndustryLabel";
-            this.IndustryLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.IndustryLabel.Size = new System.Drawing.Size(44, 13);
-            this.IndustryLabel.TabIndex = 6;
-            this.IndustryLabel.Text = "Industry";
-            // 
-            // listView1
-            // 
-            this.listView1.HideSelection = false;
-            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem5,
-            listViewItem6});
-            this.listView1.Location = new System.Drawing.Point(425, 286);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(121, 42);
-            this.listView1.TabIndex = 7;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.SmallIcon;
-            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
-            // 
-            // TrendingCheckBox
-            // 
-            this.TrendingCheckBox.AutoSize = true;
-            this.TrendingCheckBox.Location = new System.Drawing.Point(310, 334);
-            this.TrendingCheckBox.Name = "TrendingCheckBox";
-            this.TrendingCheckBox.Size = new System.Drawing.Size(122, 17);
-            this.TrendingCheckBox.TabIndex = 8;
-            this.TrendingCheckBox.Text = "Show Only Trending";
-            this.TrendingCheckBox.UseVisualStyleBackColor = true;
-            this.TrendingCheckBox.CheckedChanged += new System.EventHandler(this.TrendingCheckBox_CheckedChanged);
+            this.MinRiskLabel.AutoSize = true;
+            this.MinRiskLabel.Font = new System.Drawing.Font("Agency FB", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinRiskLabel.Location = new System.Drawing.Point(12, 9);
+            this.MinRiskLabel.Name = "MinRiskLabel";
+            this.MinRiskLabel.Size = new System.Drawing.Size(178, 42);
+            this.MinRiskLabel.TabIndex = 1;
+            this.MinRiskLabel.Text = "Minimum Risk";
+            this.MinRiskLabel.Click += new System.EventHandler(this.MinRiskLabel_Click);
             // 
             // SearchButton
             // 
-            this.SearchButton.Location = new System.Drawing.Point(308, 390);
+            this.SearchButton.Font = new System.Drawing.Font("Agency FB", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchButton.Location = new System.Drawing.Point(339, 367);
             this.SearchButton.Name = "SearchButton";
-            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.Size = new System.Drawing.Size(173, 82);
             this.SearchButton.TabIndex = 9;
             this.SearchButton.Text = "Search";
             this.SearchButton.UseVisualStyleBackColor = true;
             this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // MaxRiskLabel
+            // 
+            this.MaxRiskLabel.AutoSize = true;
+            this.MaxRiskLabel.Font = new System.Drawing.Font("Agency FB", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaxRiskLabel.Location = new System.Drawing.Point(434, 9);
+            this.MaxRiskLabel.Name = "MaxRiskLabel";
+            this.MaxRiskLabel.Size = new System.Drawing.Size(185, 42);
+            this.MaxRiskLabel.TabIndex = 10;
+            this.MaxRiskLabel.Text = "Maximum Risk";
+            this.MaxRiskLabel.Click += new System.EventHandler(this.MaxRiskLabel_Click);
+            // 
+            // MinCostLabel
+            // 
+            this.MinCostLabel.AutoSize = true;
+            this.MinCostLabel.Font = new System.Drawing.Font("Agency FB", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinCostLabel.Location = new System.Drawing.Point(12, 129);
+            this.MinCostLabel.Name = "MinCostLabel";
+            this.MinCostLabel.Size = new System.Drawing.Size(181, 42);
+            this.MinCostLabel.TabIndex = 11;
+            this.MinCostLabel.Text = "Minimum Cost";
+            this.MinCostLabel.Click += new System.EventHandler(this.MinCostLabel_Click);
+            // 
+            // MaxCostLabel
+            // 
+            this.MaxCostLabel.AutoSize = true;
+            this.MaxCostLabel.Font = new System.Drawing.Font("Agency FB", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaxCostLabel.Location = new System.Drawing.Point(431, 129);
+            this.MaxCostLabel.Name = "MaxCostLabel";
+            this.MaxCostLabel.Size = new System.Drawing.Size(188, 42);
+            this.MaxCostLabel.TabIndex = 12;
+            this.MaxCostLabel.Text = "Maximum Cost";
+            this.MaxCostLabel.Click += new System.EventHandler(this.MaxCostLabel_Click);
+            // 
+            // MinRiskTrackBar
+            // 
+            this.MinRiskTrackBar.Location = new System.Drawing.Point(196, 12);
+            this.MinRiskTrackBar.Name = "MinRiskTrackBar";
+            this.MinRiskTrackBar.Size = new System.Drawing.Size(208, 45);
+            this.MinRiskTrackBar.TabIndex = 13;
+            this.MinRiskTrackBar.Scroll += new System.EventHandler(this.MinRiskTrackBar_Scroll);
+            // 
+            // MaxRiskTrackBar
+            // 
+            this.MaxRiskTrackBar.Location = new System.Drawing.Point(625, 12);
+            this.MaxRiskTrackBar.Name = "MaxRiskTrackBar";
+            this.MaxRiskTrackBar.Size = new System.Drawing.Size(197, 45);
+            this.MaxRiskTrackBar.TabIndex = 14;
+            this.MaxRiskTrackBar.Scroll += new System.EventHandler(this.MaxRiskTrackBar_Scroll);
+            // 
+            // MinCostTrackBar
+            // 
+            this.MinCostTrackBar.Location = new System.Drawing.Point(196, 129);
+            this.MinCostTrackBar.Name = "MinCostTrackBar";
+            this.MinCostTrackBar.Size = new System.Drawing.Size(208, 45);
+            this.MinCostTrackBar.TabIndex = 15;
+            this.MinCostTrackBar.Scroll += new System.EventHandler(this.MinCostTrackBar_Scroll);
+            // 
+            // MaxCostTrackBar
+            // 
+            this.MaxCostTrackBar.Location = new System.Drawing.Point(628, 129);
+            this.MaxCostTrackBar.Name = "MaxCostTrackBar";
+            this.MaxCostTrackBar.Size = new System.Drawing.Size(194, 45);
+            this.MaxCostTrackBar.TabIndex = 16;
+            this.MaxCostTrackBar.Scroll += new System.EventHandler(this.MaxCostTrackBar_Scroll);
+            // 
+            // IndustryLabel
+            // 
+            this.IndustryLabel.AutoSize = true;
+            this.IndustryLabel.Font = new System.Drawing.Font("Agency FB", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IndustryLabel.Location = new System.Drawing.Point(76, 221);
+            this.IndustryLabel.Name = "IndustryLabel";
+            this.IndustryLabel.Size = new System.Drawing.Size(114, 42);
+            this.IndustryLabel.TabIndex = 17;
+            this.IndustryLabel.Text = "Industry";
+            this.IndustryLabel.Click += new System.EventHandler(this.IndustryLabel_Click);
+            // 
+            // IndustryInputTextBox
+            // 
+            this.IndustryInputTextBox.Font = new System.Drawing.Font("Agency FB", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IndustryInputTextBox.Location = new System.Drawing.Point(196, 225);
+            this.IndustryInputTextBox.Name = "IndustryInputTextBox";
+            this.IndustryInputTextBox.Size = new System.Drawing.Size(208, 40);
+            this.IndustryInputTextBox.TabIndex = 18;
+            this.IndustryInputTextBox.TextChanged += new System.EventHandler(this.IndustryInputTextBox_TextChanged);
+            // 
+            // ExpireDateLabel
+            // 
+            this.ExpireDateLabel.AutoSize = true;
+            this.ExpireDateLabel.Font = new System.Drawing.Font("Agency FB", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExpireDateLabel.Location = new System.Drawing.Point(434, 221);
+            this.ExpireDateLabel.Name = "ExpireDateLabel";
+            this.ExpireDateLabel.Size = new System.Drawing.Size(176, 42);
+            this.ExpireDateLabel.TabIndex = 19;
+            this.ExpireDateLabel.Text = "Expirery Date";
+            this.ExpireDateLabel.Click += new System.EventHandler(this.ExpireDateLabel_Click);
+            // 
+            // ExpireDatePicker
+            // 
+            this.ExpireDatePicker.CalendarFont = new System.Drawing.Font("Calibri", 16.25F, System.Drawing.FontStyle.Bold);
+            this.ExpireDatePicker.Font = new System.Drawing.Font("Calibri", 16.25F, System.Drawing.FontStyle.Bold);
+            this.ExpireDatePicker.Location = new System.Drawing.Point(622, 225);
+            this.ExpireDatePicker.Name = "ExpireDatePicker";
+            this.ExpireDatePicker.Size = new System.Drawing.Size(200, 34);
+            this.ExpireDatePicker.TabIndex = 20;
+            this.ExpireDatePicker.ValueChanged += new System.EventHandler(this.ExpireDatePicker_ValueChanged);
             // 
             // RelationshipManagerView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(834, 461);
-            this.Controls.Add(this.SearchButton);
-            this.Controls.Add(this.TrendingCheckBox);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.ExpireDatePicker);
+            this.Controls.Add(this.ExpireDateLabel);
+            this.Controls.Add(this.IndustryInputTextBox);
             this.Controls.Add(this.IndustryLabel);
-            this.Controls.Add(this.ExpireDateTrackBar);
-            this.Controls.Add(this.ExpiryDateLabel);
-            this.Controls.Add(this.CostTrackBar);
-            this.Controls.Add(this.CostLabel);
-            this.Controls.Add(this.RiskLabel);
-            this.Controls.Add(this.RiskTrackBar);
+            this.Controls.Add(this.MaxCostTrackBar);
+            this.Controls.Add(this.MinCostTrackBar);
+            this.Controls.Add(this.MaxRiskTrackBar);
+            this.Controls.Add(this.MinRiskTrackBar);
+            this.Controls.Add(this.MaxCostLabel);
+            this.Controls.Add(this.MinCostLabel);
+            this.Controls.Add(this.MaxRiskLabel);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.MinRiskLabel);
             this.Name = "RelationshipManagerView";
             this.Text = "RelationshipManagerView";
             this.Load += new System.EventHandler(this.RelationshipManagerView_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.RiskTrackBar)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.CostTrackBar)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ExpireDateTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MinRiskTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MaxRiskTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MinCostTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MaxCostTrackBar)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TrackBar RiskTrackBar;
-        private System.Windows.Forms.Label RiskLabel;
-        private System.Windows.Forms.Label CostLabel;
-        private System.Windows.Forms.TrackBar CostTrackBar;
-        private System.Windows.Forms.Label ExpiryDateLabel;
-        private System.Windows.Forms.TrackBar ExpireDateTrackBar;
-        private System.Windows.Forms.Label IndustryLabel;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.CheckBox TrendingCheckBox;
+        private System.Windows.Forms.Label MinRiskLabel;
         private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Label MaxRiskLabel;
+        private System.Windows.Forms.Label MinCostLabel;
+        private System.Windows.Forms.Label MaxCostLabel;
+        private System.Windows.Forms.TrackBar MinRiskTrackBar;
+        private System.Windows.Forms.TrackBar MaxRiskTrackBar;
+        private System.Windows.Forms.TrackBar MinCostTrackBar;
+        private System.Windows.Forms.TrackBar MaxCostTrackBar;
+        private System.Windows.Forms.Label IndustryLabel;
+        private System.Windows.Forms.TextBox IndustryInputTextBox;
+        private System.Windows.Forms.Label ExpireDateLabel;
+        private System.Windows.Forms.DateTimePicker ExpireDatePicker;
     }
 }
--- RelationshipManagerView.cs
+++ RelationshipManagerView.cs
@@ -22,32 +22,69 @@ namespace aru_software_eng_UI
 
         }
 
-        private void RiskTrackBar_Scroll(object sender, EventArgs e)
-        {
-
-        }
-
-        private void CostTrackBar_Scroll(object sender, EventArgs e)
-        {
-
-        }
-
-        private void ExpireDateTrackBar_Scroll(object sender, EventArgs e)
-        {
-
-        }
-
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TrendingCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
+       
 
         private void SearchButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MaxRiskLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MinRiskLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MinCostLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MaxCostLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MinRiskTrackBar_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MinCostTrackBar_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MaxRiskTrackBar_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MaxCostTrackBar_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExpireDatePicker_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExpireDateLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IndustryInputTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IndustryLabel_Click(object sender, EventArgs e)
         {
 
         }
