c3346905c9dc0b71f730ae3c365e80c55a59a185
btabtab <bladethace@gmail.com>
2022-12-09 00:03:22 +0000

    FINALLLLYYYYY FREEEEDOMM!!

--- CreateNewIdea.cs
+++ CreateNewIdea.cs
@@ -10,36 +10,47 @@ using System.Windows.Forms;
 
 namespace aru_software_eng_UI
 {
-    public partial class CreateNewIdeaForm : Form
-    {
+	public partial class CreateNewIdeaForm : Form
+	{
 
-        FormManager manager;
-        DataBaseLoginEntry user;
-        public CreateNewIdeaForm(Form previous_form, DataBaseLoginEntry n_user)
-        {
-            InitializeComponent();
-            manager = new FormManager(previous_form, this);
-            user = n_user;
-        }
-        private void IS_back_button_Click(object sender, EventArgs e)
-        {
-            manager.back();
-        }
-        private void cancel_button_Click(object sender, EventArgs e)
-        {
-            manager.back();
-        }
+		FormManager manager;
+		DataBaseLoginEntry user;
+		public CreateNewIdeaForm(Form previous_form, DataBaseLoginEntry n_user)
+		{
+			InitializeComponent();
+			manager = new FormManager(previous_form, this);
+			user = n_user;
+		}
+		private void IS_back_button_Click(object sender, EventArgs e)
+		{
+			manager.back();
+		}
+		private void cancel_button_Click(object sender, EventArgs e)
+		{
+			manager.back();
+		}
 
-        private void IS_save_button_Click(object sender, EventArgs e)
-        {
-            InvestmentIdeaDatabaseHandler.getInstance().writeInvestmentIdea(new InvestmentIdea(date_picker.Value, name.Text, desc.Text, InvestmentIdeaDatabaseHandler.getInstance().getHighestID("ID"), user.getID(), Cost.Value, Risk.Value, product_tag.Text, user.getRMPermissionLevel()));
-            manager.back();
-        }
+		private void IS_save_button_Click(object sender, EventArgs e)
+		{
+			InvestmentIdeaDatabaseHandler.getInstance().writeInvestmentIdea(new InvestmentIdea(
+																							date_picker.Value,
+																							name.Text,
+																							desc.Text,
+																							InvestmentIdeaDatabaseHandler.getInstance().getHighestID("ID"),
+																							user.getID(),
+																							Cost.Value,
+																							Risk.Value,
+																							product_tag.Text,
+																							user.getRMPermissionLevel()
+																							)
+																			);
+			manager.back();
+		}
 
-        private void IS_back_button_Click_2(object sender, EventArgs e)
-        {
-            manager.back();
-        }
+		private void IS_back_button_Click_2(object sender, EventArgs e)
+		{
+			manager.back();
+		}
 
-    }
+	}
 }
